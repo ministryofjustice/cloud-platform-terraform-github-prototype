656094d020497f4d4f4e5c53646a2c8cf7961319
jakemulley <jakemulley@users.noreply.github.com>
2022-08-19 14:42:19 +0100

    Remove unused providers and variables, add missing variable types

--- versions.tf
+++ versions.tf
@@ -2,10 +2,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 4.14.0"
-    }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
+      version = ">= 4.14.0"
     }
   }
   required_version = ">= 0.14"
